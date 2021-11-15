6820c595cfd2af4bbaf7f0d38a5becacea0a1f79
Miłosz Lewandowski <m.lewandowski@nebucode.com>
2021-11-15 16:16:40 +0100

    Initial commit

---
+++ src/main/java/pl/miloszlewandowski/moodchecksapi/MoodchecksApiApplication.java
@@ -0,0 +1,13 @@
+package pl.miloszlewandowski.moodchecksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodchecksApiApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(MoodchecksApiApplication.class, args);
+  }
+  
+}
---
+++ src/test/java/pl/miloszlewandowski/moodchecksapi/MoodchecksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.miloszlewandowski.moodchecksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodchecksApiApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
